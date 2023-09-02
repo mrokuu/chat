c94c941738fffe3660095f9ae0d2f6fc539aae53
mrokuu <mroczkowskiadammm@gmail.com>
2023-09-02 21:00:18 +0200

    first commit

---
+++ src/main/java/com/chat/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.chat.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType messageType;
+}
---
+++ src/main/java/com/chat/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.chat.chat.chat;
+
+public enum MessageType {
+
+        CHAT,
+        JOIN,
+        LEAVE
+}
